71804928aa538a01df8ba50dff107968367a2d3f
∇×𝐸 <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-09-01 11:45:56 +0530

    .

--- Lab3/Examples/Ex1/Q1ARM.s
+++ Lab3/Examples/Ex1/Q1ARM.s
@@ -1,4 +1,5 @@
-        AREA RESET, DATA, READONLY
+;Write an assembly program to convert a 2 digit hexadecimal number into unpacked ASCII. 
+		AREA RESET, DATA, READONLY
         EXPORT __Vectors
 
 __Vectors
@@ -11,68 +12,56 @@ __Vectors
         ENTRY
         EXPORT Reset_Handler
 Reset_Handler
-		LDR 	R7, =NUM1
-		LDR		R0, [R7]
-		LDR		R8, =RESULT
 		
-		;extract lower byte:
-		MOV		R1, #0x0F
-		
-		AND		R1, R1, R0
-		
-		
-		;extract upper byte
-		MOV		R2, #0xF0
-		
-		AND		R2, R2, R0
-		
-		LSR		R2, R2, #4
-		
-		;ADD 30 to lower byte if its between 0 to 9, otherwise add 37
-		
-		CMP		R1, #9
-		
-		BGT		ADD_37_lower
-		B		ADD_30_lower
-		
-ADD_37_lower
-		ADD		R1, R1, #0x37
-		B		lower_done
-ADD_30_lower
-		ADD		R1, R1, #0x30
-lower_done
-
-		;ADD 30 to upper byte if its between 0 to 9, otherwise add 37
+		LDR		R0,=NUM1
+		LDR		R1,[R0]
+		LDR		R5, =RESULT
+		;extract lower nibble:
+		MOV		R2, #0x0F
+		AND		R2, R2, R1
 		
 		CMP		R2, #9
-
-		BGT		ADD_37_upper
-		B		ADD_30_upper
 		
-ADD_37_upper
-		ADD		R2, R2, #0x37
-		B		upper_done
-ADD_30_upper
+		BGT		ADD_37_lower
+		BLE		ADD_30_lower
+		
+ADD_30_lower
 		ADD		R2, R2, #0x30
-upper_done		
+		B		SECOND
+ADD_37_lower
+		ADD		R2, R2, #0x37
+SECOND
+
+		;extract upper nibble:
 		
-		LSL		R2,R2,#8
+		MOV		R3, #0xF0
+		AND		R3, R3, R1
+		LSR		R3, R3, #4
 		
-		;final answer:
+		CMP		R3, #9
 		
-		ORR 	R3, R1, R2
+		BGT		ADD_37_upper
+		BLE		ADD_30_upper
 		
-		STR		R3, [R8]
+ADD_30_upper
+		ADD		R3, R3, #0x30
+		B		DONE
+ADD_37_upper
+		ADD		R3, R3, #0x37
+		
+DONE
+		STRB	R2, [R5]
+		STRB	R3, [R5,#1]
 		
 STOP
 		B		STOP
 		
 		AREA	mydata, DATA, READWRITE
 RESULT
-		
-		SPACE 4
+		SPACE	8
 		
 		AREA	constdata, DATA, READONLY
-NUM1	DCD		0x21
-	
+NUM1
+		DCD		0x21
+			
 		END
