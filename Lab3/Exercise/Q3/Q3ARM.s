6ae6d4687d34b40647e4be2d200ea6e1a2c55bba
∇×𝐸 <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-09-01 21:22:01 +0530

    Refactor ARM assembly code for number processing

--- Lab3/Exercise/Q3/Q3ARM.s
+++ Lab3/Exercise/Q3/Q3ARM.s
@@ -3,7 +3,7 @@
 
 __Vectors
         DCD 0x10001000             ; stack pointer value when stack is empty
-        DCD Reset_Handler          ; reset vector
+        DCD Reset_Handler           ; reset vector
 
         ALIGN
 
@@ -11,47 +11,55 @@ __Vectors
         ENTRY
         EXPORT Reset_Handler
 Reset_Handler
-		
+
 		LDR		R0,=NUM1
-		LDR		R1,[R0] ;R1 = number
-		LDR 	R8, =RESULT
-		;extract lower nibble
+		LDRH	R1,[R0]
+		LDR		R10,=RESULT
+		;extracting lower nibble:
 		
 		MOV		R2, #0x0F
-		AND		R2, R2, R1 ;R2 = lower nibble
+		AND		R2, R2, R1
 		
-		;extract upper nibble
+		;extracting upper nibble:
 		
 		MOV		R3, #0xF0
 		AND		R3, R3, R1
-		LSR		R3, R3, #4 ;R3 = upper nibble
+		LSR		R3, R3, #4
 		
-		MOV 	R4, #16
-		MUL		R3, R3, R4 ;R3 = 16 * upper nibble
-		ADD		R5, R3, R2 ;R5 = 16*upper nibble + lower nibble
+		MOV		R4, #16
+		MUL		R3, R3, R4
+		ADD		R5, R3, R2
 		
-		MOV 	R2, #10
-		UDIV 	R6, R5, R2
-		MUL		R2, R2, R6
-		SUB		R5, R5, R2
+		MOV		R1, #10
+		MOV		R2, #100
+		;R5 = decimal number basically..
 		
-		;R6 = quotient, upper nibble
-		;R5 = remainder, lower nibble
+		UDIV	R6, R5, R2
+		MUL		R3, R2, R6
+		SUB		R7, R5, R3
 		
-		LSL		R6, R6, #4
+		STRB	R6,[R10]
 		
-		ORR		R7, R6, R5
+		MOV		R5, R7
 		
-		STR		R7, [R8]
+		UDIV	R6, R5, R1
+		MUL		R3, R1, R6
+		SUB		R7, R5, R3
+		
+		STRB		R6,[R10, #1]
+		
+		STRB		R7,[R10, #2]
+		
 STOP
 		B		STOP
 		
 		AREA	mydata, DATA, READWRITE
-RESULT	
-		SPACE	4
+RESULT
+		SPACE	3
 		
 		AREA	constdata, DATA, READONLY
+NUM1
+		DCD		0x21
 			
-NUM1	DCD		0x25
-	
 		END
+		
